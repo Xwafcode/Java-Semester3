42c16ca1365f1a8091bf3be27c9e22c4f2339b74
Xwafcode <trxhgxng@gmail.com>
2024-10-22 19:19:42 +0700

    Second push

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Git!");
+    public static void main(String[]args){
+        String text ="Hello,Maven!";
+        String reversedText = StringUtils.reverse(text);
+        System.out.println(reversedText);
     }
 }
